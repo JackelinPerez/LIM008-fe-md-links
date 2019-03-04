57aab588d934c15070006077e42a46e095dcc224
JackelinPerez <filomenochahua@gmail.com>
2019-03-04 10:08:53 -0500

    inlude get all links

--- src/controllers.js
+++ src/controllers.js
@@ -1,24 +1,22 @@
-import {getAllMds} from "./controllers/getAllMd.js";
+import {getAllFilesMd} from "./controllers/getAllMd.js";
 let statsOrValidate = [];
 const dir_ = process.argv[2];
-const exten_ = process.argv[3];
 
 statsOrValidate[0] = process.argv[4];
 statsOrValidate[1] = process.argv[5];
 
 
-const mdLinks = (dir, exten, statsOrValidate) =>{
+const mdLinks = (dir, statsOrValidate) =>{
 	return new Promise ((resolve, reject) =>{
 		try {
-            let contAllFile = [];
-            return resolve(getAllMds(dir, exten, contAllFile, 0, 0))
+            return resolve(getAllFilesMd(dir, '.md',[]))
 		}
 		catch (err){
 			reject(err);
 		}    
 });
-
-mdLinks(dir_, exten_, statsOrValidate)
+}
+mdLinks(dir_, statsOrValidate)
 .then((resolve) =>{
     console.log(resolve);
 })
--- src/controllers/getAllMd.js
+++ src/controllers/getAllMd.js
@@ -1,48 +1,19 @@
-export const filtroDirAndFile = (document, extFile, dir, fs) => {
-    const objMdAndDir = {
-      file : [],
-      dir: []
-    };
-    return document.reduce((anterior, doc) => {
-      const rutaAllDoc = `${dir}\\${doc}`;
-      const validateDir = fs.lstatSync(rutaAllDoc).isDirectory();
-      if(validateDir) objMdAndDir.dir.push(rutaAllDoc);
-      else if (doc.toLowerCase().indexOf(extFile)!==-1) objMdAndDir.file.push(rutaAllDoc);
-      return objMdAndDir;
-    }, {});
-}
-
-export const getAllMds= (dir, exten, contAllFile, contMain, lengthMain) => {
-    let auxObjMdAndDir = {};
-    const fs = require('fs');
-    const validateDir = fs.lstatSync(dir).isDirectory();
-  
-    if (validateDir) {
-      const items= fs.readdirSync(dir);
-      if(items.length>0){
-        auxObjMdAndDir = filtroDirAndFile(items, exten, dir, fs);
-        if(auxObjMdAndDir.file.length > 0)
-        auxObjMdAndDir.file.forEach((fileMD) => contAllFile.push(fileMD));
-        
-        if (auxObjMdAndDir.dir.length > 0) {
-            auxObjMdAndDir.dir.forEach((dirEle, index) => {
-              if(lengthMain === 0 && contMain ===0) lengthMain = auxObjMdAndDir.dir.length;
-              if(index === contMain) contMain++;
-              getAllMds(dirEle, exten, contAllFile, contMain);
-            });     
-        }
-        else console.log('No hay + Directorios: '+dir);
-      }
-      else console.log('No hay hijos en: '+dir);
+export const getAllFilesMd= (dirAbsOrRel, extenFile, savefilesMD) => {
+  const paths = require('path');
+  const fs = require('fs');
+  try {
+    if (fs.statSync(dirAbsOrRel).isDirectory()) {
+      let arrayChildDir = fs.readdirSync(dirAbsOrRel);
+      arrayChildDir.forEach((childDir) => {
+      let dirAbsOrRelChild = paths.join(dirAbsOrRel, childDir);
+      getAllFilesMd(dirAbsOrRelChild, extenFile, savefilesMD);		
+      });
     }
-    else {
-      const fileInput = [dir.substr(dir.lastIndexOf('\\')+1,dir.length)];
-      const dirFileInput = dir.substr(0,dir.lastIndexOf('\\'));
-      return filtroDirAndFile(fileInput,exten,dirFileInput, fs).file;
-    }
-  
-      if(lengthMain === contMain) return contAllFile;
-      return 0;
-}
-  
-
+    else if (paths.extname(dirAbsOrRel) === extenFile)
+      savefilesMD.push(dirAbsOrRel);		  
+    return savefilesMD;	
+  }
+  catch (err) {
+    console.log(err.message);
+  }
+};
